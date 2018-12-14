ec7fab096eeb8cba2e25e0862a7223e2e60ce7e9
sergi grau <sergi.grau@fje.edu>
2018-12-14 12:43:15 +0100

    materials especifics

---
+++ app/src/main/java/edu/fje/dam2/androidthings/M05_LecturaDirecteBMX280.kt
@@ -0,0 +1,53 @@
+package edu.fje.dam2.androidthings
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.Handler
+import com.google.android.things.contrib.driver.bmx280.Bmx280
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
+import com.google.android.things.contrib.driver.ht16k33.Ht16k33
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+
+class M05_LecturaDirecteBMX280: Activity() {
+
+    private val handler = Handler()
+
+    private lateinit var sensor: Bmx280
+    private lateinit var alphanumericDisplay: AlphanumericDisplay
+
+    val filMostrarTemperatura = object: Runnable {
+        override fun run() {
+            val temperatura = sensor.readTemperature().toDouble()
+            alphanumericDisplay.display(temperatura)
+            handler.post(this)
+        }
+    }
+    val filMostrarPressio = object: Runnable {
+        override fun run() {
+            val pressure = sensor.readPressure().toDouble()
+            alphanumericDisplay.display(pressure)
+            handler.post(this)
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sensor = RainbowHat.openSensor()
+        sensor.temperatureOversampling = Bmx280.OVERSAMPLING_1X
+        sensor.setMode(Bmx280.MODE_NORMAL)
+        sensor.pressureOversampling = Bmx280.OVERSAMPLING_1X
+
+        alphanumericDisplay = RainbowHat.openDisplay()
+        alphanumericDisplay.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX)
+        alphanumericDisplay.setEnabled(true)
+
+        handler.post(filMostrarPressio)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(filMostrarPressio)
+        sensor.close()
+        alphanumericDisplay.close()
+    }
+}
---
+++ app/src/main/java/edu/fje/dam2/androidthings/M06_AlarmaActivity.java
@@ -0,0 +1,115 @@
+package edu.fje.dam2.androidthings;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.ht16k33.Ht16k33;
+import com.google.android.things.contrib.driver.pwmspeaker.Speaker;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+import com.google.android.things.pio.Gpio;
+
+import java.io.IOException;
+
+
+/**
+ * Activitat de Android Things.
+ * Es tracta d'una alarma compte enrera.
+ * <p>
+ * Els periferics de Android Things peripheral s'accedeixen amb la classe PeripheralManagerService.
+ * <p>
+ * @author sergi.grau@fje.edu
+ * @version 1.0 30.11.2018
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class M06_AlarmaActivity extends Activity {
+
+    private int comptador;
+    private Gpio ledVermell;
+    private ButtonInputDriver botoA, botoB;
+    private AlphanumericDisplay segment;
+    private  Speaker altaveu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            ledVermell = RainbowHat.openLedRed();
+            segment = RainbowHat.openDisplay();
+            segment.clear();
+            segment.setEnabled(true);
+            altaveu = RainbowHat.openPiezo();
+
+            botoA = RainbowHat.createButtonAInputDriver(KeyEvent.KEYCODE_A);
+            botoB = RainbowHat.createButtonBInputDriver(KeyEvent.KEYCODE_B);
+            botoA.register();
+            botoB.register();
+
+        }
+        catch (Exception e){
+
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_A) {
+
+            try {
+                segment.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX);
+                segment.display(++comptador);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+         if (keyCode == event.KEYCODE_B) {
+            for (int i = comptador; i >= 0; i--) {
+                try {
+                    altaveu.play(10.0);
+                    segment.setBrightness(5);
+                    segment.display(i);
+                    SystemClock.sleep(100);
+                    altaveu.stop();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+        else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+        @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try{
+            botoA.unregister();
+            botoB.unregister();
+            ledVermell.close();
+            altaveu.close();
+            segment.clear();
+        }
+        catch (Exception e){
+
+        }
+
+    }
+}
